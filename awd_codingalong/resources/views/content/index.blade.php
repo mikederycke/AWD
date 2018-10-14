daed585d6b63a56e899da2c7b28a951a68bf0bb5
mikederycke <mike.derycke@ehb.be>
2018-10-14 22:58:40 +0200

    Coding along Stap 3

---
+++ awd_codingalong/resources/views/content/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">Zoekertje 1</h1>
+            <p class="lead">Boardgames</p>
+            <hr class="my-4">
+            <p>Verzameling van allerhande boardgames</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Details</a>
+        </div>
+        <div class="jumbotron">
+            <h1 class="display-4">Zoekertje 2</h1>
+            <p class="lead">Gitaar</p>
+            <hr class="my-4">
+            <p>Les Paul Strat van 1965. Uitstekende staat!</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Details</a>
+        </div>
+        <div class="jumbotron">
+            <h1 class="display-4">Zoekertje 3</h1>
+            <p class="lead">Handboek ReactJS</p>
+            <hr class="my-4">
+            <p>Be a pro in React in NO TIME!</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Details</a>
+        </div>
+    </div>
+@endsection
